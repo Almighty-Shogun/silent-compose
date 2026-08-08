7c6f7b63b13a47557f88007ea14c4c33a4604662
Shogun <alm1ghtyshogun1998@gmail.com>
2026-08-08 15:27:42 +0200

    fix: enable AltGr layout variant

---
+++ tests/test-component.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+set -eu
+
+if [ "$#" -ne 1 ]; then
+  echo "usage: test-component.sh COMPONENT_XML" >&2
+  exit 2
+fi
+
+component_xml=$1
+
+grep -Fx "      <layout>us</layout>" "${component_xml}" >/dev/null
+grep -Fx "      <layout_variant>altgr-intl</layout_variant>" "${component_xml}" >/dev/null
